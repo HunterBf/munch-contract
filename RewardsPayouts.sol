a85aeeff076592ca2ada17a0434da2804db501b2
HunterBf <49005701+HunterBf@users.noreply.github.com>
2020-06-10 22:17:54 -0400

    updated muncherpayout with burn features instead of transfer

--- RewardsPayouts.sol
+++ RewardsPayouts.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.6.6;
 import "./Muncher.sol";
+
 contract RewardsPayouts is MunchCommunity {
-    
+
+    uint basePay = 20 * (10**18);
     //we want to show users when they recieve their $MNCH and where they got it from.
     event paymentNotification(string eateryname, string rewardamount);
     
@@ -20,24 +22,26 @@ contract RewardsPayouts is MunchCommunity {
     }
     
     //should be in $MNCH but this is the rewards system.
-    function muncherPayOut(uint _id, uint _eateryId) external returns(string memory) {
+    function muncherPayout(uint _id, uint _eateryId) public returns (bool success) {
         require(msg.sender == muncherToOwner[_id]);
         _id = _id.sub(1);
         _eateryId = _eateryId.sub(1);
-        require(now >= munchers[_id].withdrawTime, "unable to send payout, you just recently got one.");
+        require(now >= munchers[_id].withdrawTime, "unable to send payout, you just recently got one.");     // Only allow payout every 8 hours
         eateries[_eateryId].munchers = eateries[_eateryId].munchers.add(1);
-        increaseAllowance(address(this), 20 *(10**18));
+        // mints new tokens accordingly
         if (eateries[_eateryId].certified == true && eateries[_eateryId].local == true) {
-            transferFrom(address(this), msg.sender, 20 *(10**18));
+            _mint(msg.sender, basePay); 
             emit paymentNotification(eateries[_eateryId].name, "you've recived 20 $MNCH!");
         } else if (eateries[_eateryId].certified == false && eateries[_eateryId].local == true) {
-            transferFrom(address(this), msg.sender, 15 *(10**18));
+            _mint(msg.sender, (basePay * 3) / 2);
             emit paymentNotification(eateries[_eateryId].name, "you've recived 15 $MNCH!");
         } else { //chains & nonlocal eateries
-            transferFrom(address(this), msg.sender, 10 *(10**18));
+            _mint(msg.sender, basePay / 2);
             emit paymentNotification(eateries[_eateryId].name, "you've recived 10 $MNCH!");
         } 
         munchers[_id].withdrawTime = now.add(rewardclock);
-        return "success!";
-    } 
+        return true;
+    }
+    
+    // we recieve an input from plaid, which technically must have a hex address. We only grant that hex address access to send $MNCH from
 }
