dd9af1a8f534a71b4a21ab39750a1e8344b1d5ff
bfreshhb <49005701+bfreshhb@users.noreply.github.com>
2020-06-11 22:17:41 -0400

    Update Muncher.sol
    added a referral function and adjusted rankup fee so it's in $MNCH rather than ETH.

--- Muncher.sol
+++ Muncher.sol
@@ -4,7 +4,8 @@ contract MunchCommunity is EateryInfo, RockToken {
     
     uint muncherId;
     uint rewardclock = 8 hours;
-    uint rankUpFee = 0.001 ether;
+    uint rankUpFee = 100 * (10**18);
+    uint referralAmount = 5 * (10**18);
    
    //a way to store all of our munchers on the blockchain!
     struct Muncher {
@@ -20,19 +21,25 @@ contract MunchCommunity is EateryInfo, RockToken {
     mapping (uint => address) public muncherToOwner;
     
     //a function to make a muncher and push it to an array.
-    function muncherCreator(string calldata _nickname) external {
+    function muncherCreator(string memory _nickname) public {
         muncherId = muncherId.add(1);
         muncherToOwner[muncherId] = msg.sender;
         munchers.push(Muncher(_nickname, "munchie", 1, muncherId, now));
     }
     
+    function muncherReferral(uint _muncherId, string memory _nickname) public {
+        //may need to be a limitation on this...
+        muncherCreator(_nickname);
+        _mint(msg.sender, referralAmount);
+        _mint(muncherToOwner[_muncherId], referralAmount);
+    }
+    
     //rank up after acquiring a certain amount of $MNCH.
     function rankUp(uint _id) external payable {
         require(msg.sender == muncherToOwner[_id]);
-        burn(20 * (10**18));
+        burn(rankUpFee);
         //require(msg.value == rankUpFee, "wrong amount of rockies provided");
         Muncher storage myMuncher = munchers[_id.sub(1)];
         myMuncher.rank = myMuncher.rank.add(1); //finding the muncher from the array and increasing its rank.
     }
-    
 }
