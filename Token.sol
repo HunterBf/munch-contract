ef6910ed6dc99b249a2a1099d8df0dd51f34e32e
bfreshhb <49005701+bfreshhb@users.noreply.github.com>
2020-06-13 21:03:30 -0400

    changed import from local to github

--- Token.sol
+++ Token.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.6.6;
-import "./openzeppelin-contracts-master/contracts/token/ERC20/ERC20Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract RockToken is ERC20Burnable {
+contract MunchToken is ERC20Burnable {
     
-    constructor() public ERC20 ("Rockies", "ROCK") {
+    constructor() public ERC20 ("Munchcoin", "MNCH") {
         _mint(msg.sender, 9999999 *10**18);
         // we multiply by 10^18 because there are that many decimal places.
     }
