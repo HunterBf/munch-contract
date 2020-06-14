dc7b50137272b51c6d4abacd5f78f243a39bf560
bfreshhb <49005701+bfreshhb@users.noreply.github.com>
2020-06-13 21:02:14 -0400

    Updated Muncher.sol
    Added quite alot... mainly organized and fixed things. May be too many mappings btw but oh well. I'll clean it up later I guess

--- Muncher.sol
+++ Muncher.sol
@@ -1,45 +1,67 @@
 pragma solidity ^0.6.6;
 import "./Eatery.sol";
-contract MunchCommunity is EateryInfo, RockToken {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
+contract MunchCommunity is EateryInfo, MunchToken {
+    
+    using Strings for uint;
+    
+    //we want to show users when they recieve their $MNCH and where they got it from.
+    event paymentNotification(string eateryname, string rewardamount);
     
     uint muncherId;
+    uint muncherIdArray;
     uint rewardclock = 8 hours;
     uint rankUpFee = 100 * (10**18);
     uint referralAmount = 5 * (10**18);
    
    //a way to store all of our munchers on the blockchain!
     struct Muncher {
-        string nickname;
+        string munchname;
         string rankname;
+        bool isamuncher;
+        bool referral;
         uint8 rank;
         uint id;
         uint withdrawTime;
     }
     
-    Muncher[] public munchers;
+    Muncher[] internal munchers;
     
+    //may be too many mappings..
     mapping (uint => address) public muncherToOwner;
+    mapping (string => address) public muncherNameToOwner;
+    mapping (address => Muncher) public addressToMuncher;
+    mapping (string => uint) public muncherNameToId;
     
-    //a function to make a muncher and push it to an array.
+    //function to put together a muncher's name.
+    function munchNameMaker(string memory _nickname) internal view returns(string memory) {
+        return string(abi.encodePacked(_nickname,"#",muncherId.toString()));
+    }
+    
+    //function to make a muncher and push it to an array.
     function muncherCreator(string memory _nickname) public {
+        require(addressToMuncher[msg.sender].isamuncher != true, "You're already a muncher!");
+        muncherIdArray = muncherId;
         muncherId = muncherId.add(1);
+        string memory munchname = munchNameMaker(_nickname);
+        munchers.push(Muncher(munchname, "munchie", true, false, 1, muncherId, now));
         muncherToOwner[muncherId] = msg.sender;
-        munchers.push(Muncher(_nickname, "munchie", 1, muncherId, now));
+        muncherNameToOwner[munchname] = msg.sender;
+        addressToMuncher[msg.sender] = munchers[muncherIdArray];
+        muncherNameToId[munchname] = muncherId;
     }
     
-    function muncherReferral(uint _muncherId, string memory _nickname) public {
-        //may need to be a limitation on this...
-        muncherCreator(_nickname);
-        _mint(msg.sender, referralAmount);
-        _mint(muncherToOwner[_muncherId], referralAmount);
-    }
-    
-    //rank up after acquiring a certain amount of $MNCH.
-    function rankUp(uint _id) external payable {
-        require(msg.sender == muncherToOwner[_id]);
-        burn(rankUpFee);
-        //require(msg.value == rankUpFee, "wrong amount of rockies provided");
-        Muncher storage myMuncher = munchers[_id.sub(1)];
-        myMuncher.rank = myMuncher.rank.add(1); //finding the muncher from the array and increasing its rank.
+    //may need to be a limitation on this...
+    function muncherReferral(string memory _existingmuncher, string memory _nickname) public {
+        if (muncherNameToId[_existingmuncher] > 0) {
+            muncherCreator(_nickname);
+            _mint(msg.sender, referralAmount);
+            _mint(muncherNameToOwner[_existingmuncher], referralAmount);
+            uint myArrayId = addressToMuncher[msg.sender].id - 1;
+            munchers[myArrayId].referral = true;
+            emit paymentNotification("Referral", "5 $MNCH");
+        } else {
+            revert("The muncher who referred you doesn't exist!");
+        }
     }
 }
