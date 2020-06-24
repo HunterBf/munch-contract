9064b9f982d7cf39d297cf62fc86e75311325c55
bfreshhb <49005701+bfreshhb@users.noreply.github.com>
2020-06-24 08:52:32 -0400

    Removed RankUp functionality

--- RewardsPayouts.sol
+++ RewardsPayouts.sol
@@ -4,20 +4,6 @@ contract RewardsPayouts is MunchCommunity {
 
     uint basePay = 20 * (10**18);
 
-    // Rewarding users with different rank names when they rank up high enough.
-    modifier rankReward(uint _rank, uint _id) {
-        require(msg.sender == muncherToOwner[_id]);
-        _;
-    }
-    
-    function muncherRankReward5(uint _id) external rankReward(5 *(10**18), _id) {
-        munchers[_id].rankname = "muncher";
-    }
-    
-    function muncherRankReward10(uint _id) external rankReward(10 *(10**18), _id) {
-        munchers[_id].rankname = "munchboy";
-    }
-    
     //should be in $MNCH but this is the rewards system.
     function muncherPayout(uint _id, uint _eateryId) public returns (bool success) {
         require(msg.sender == muncherToOwner[_id]);
@@ -35,16 +21,9 @@ contract RewardsPayouts is MunchCommunity {
         } else { //chains & nonlocal eateries
             _mint(msg.sender, basePay / 2);
             emit paymentNotification(eateries[_eateryId].name, "you've recived 10 $MNCH!");
+            // change above b/c the eatery wont be in our system to take the name from.
         } 
         munchers[_id].withdrawTime = now.add(rewardclock);
         return true;
-    }
-    
-    //rank up after acquiring a certain amount of $MNCH.
-    function rankUp(uint _id) external payable {
-        require(msg.sender == muncherToOwner[_id]);
-        burn(rankUpFee);
-        Muncher storage myMuncher = munchers[_id.sub(1)];
-        myMuncher.rank = myMuncher.rank.add(1); //finding the muncher from the array and increasing its rank.
     }
 }
