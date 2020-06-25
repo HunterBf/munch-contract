97b205ffa2211ca74344dfb6014afd71c5288248
bfreshhb <49005701+bfreshhb@users.noreply.github.com>
2020-06-24 21:58:17 -0400

    Updated RewardsPayouts.sol
    - basepay is now able to be modified by us
    - payout function is a little simpler
    - referralpayout function is in the works

--- RewardsPayouts.sol
+++ RewardsPayouts.sol
@@ -4,26 +4,35 @@ contract RewardsPayouts is MunchCommunity {
 
     uint basePay = 20 * (10**18);
 
-    //should be in $MNCH but this is the rewards system.
-    function muncherPayout(uint _id, uint _eateryId) public returns (bool success) {
-        require(msg.sender == muncherToOwner[_id]);
-        _id = _id.sub(1);
-        _eateryId = _eateryId.sub(1);
-        require(now >= munchers[_id].withdrawTime, "unable to send payout, you just recently got one.");     // Only allow payout every 8 hours
-        eateries[_eateryId].munchers = eateries[_eateryId].munchers.add(1);
+    function changeBasePay(uint _basePay) public onlyOwner returns(uint) {
+        basePay = _basePay * (10**18);
+    return basePay;
+    }
+    
+    //will need chainlink/plaid integration here
+    function muncherPayout(address _muncherAddress, uint _eateryId) public onlyOwner returns (bool success) {
+        require(now >= addressToMuncher[_muncherAddress].withdrawTime, "unable to send payout, you just recently got one.");     // Only allow payout every 8 hours
+        eateryIndex[_eateryId].munchers = eateryIndex[_eateryId].munchers.add(1);
         // mints new tokens accordingly
-        if (eateries[_eateryId].certified == true && eateries[_eateryId].local == true) {
+        if (eateryIndex[_eateryId].certified == true && eateryIndex[_eateryId].local == true) {
             _mint(msg.sender, basePay); 
-            emit paymentNotification(eateries[_eateryId].name, "you've recived 20 $MNCH!");
-        } else if (eateries[_eateryId].certified == false && eateries[_eateryId].local == true) {
+            emit paymentNotification(eateryIndex[_eateryId].name, "you've recived 20 $MNCH!");
+        } else if (eateryIndex[_eateryId].certified == false && eateryIndex[_eateryId].local == true) {
             _mint(msg.sender, (basePay * 3) / 2);
-            emit paymentNotification(eateries[_eateryId].name, "you've recived 15 $MNCH!");
+            emit paymentNotification(eateryIndex[_eateryId].name, "you've recived 15 $MNCH!");
         } else { //chains & nonlocal eateries
             _mint(msg.sender, basePay / 2);
-            emit paymentNotification(eateries[_eateryId].name, "you've recived 10 $MNCH!");
+            emit paymentNotification(eateryIndex[_eateryId].name, "you've recived 10 $MNCH!");
             // change above b/c the eatery wont be in our system to take the name from.
         } 
-        munchers[_id].withdrawTime = now.add(rewardclock);
+       addressToMuncher[_muncherAddress].withdrawTime = now.add(rewardclock);
         return true;
     }
-}
+    
+    // not done yet
+   // function referralPayout() {
+   //   _mint(msg.sender, referralAmount);
+   //     _mint(nameToMuncher[_existingmuncher].muncherAddress, referralAmount);
+   // emit paymentNotification("Referral", "5 $MNCH");
+    //}
+} 
