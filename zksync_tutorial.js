23f8c3524a1b5d508af379125dbf192714aff85f
Christopher Dubois <christopherdubois@Frantzs-Mini.fios-router.home>
2020-07-01 21:08:54 -0400

    adding zksync tutorial

---
+++ zksync_tutorial.js
@@ -0,0 +1,14 @@
+async function zksync_tut() {
+    const zksync_1 = require("zksync");
+    const ethers_1  = require("ethers");
+    const syncProvider = await zksync_1.getDefaultProvider('rinkeby');
+    const ethersProvider = new ethers_1.getDefaultProvider('rinkeby');
+  
+    const ethWallet = ethers_1.Wallet.fromMnemonic("MNEMONIC").connect(ethersProvider);
+    const syncWallet = await zksync_1.Wallet.fromEthSigner(ethWallet, syncProvider);
+  
+    console.log("Hello");
+  }
+  
+  zksync_tut();
+  
